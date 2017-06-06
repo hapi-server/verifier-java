3eb57aeee4b2aab512f111ded75bc5802540be46
jbf <faden@cottagesystems.com>
2017-06-06 11:18:03 -0500

    trivial comments

--- src/java/com/cottagesystems/CatalogCheck.java
+++ src/java/com/cottagesystems/CatalogCheck.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.cottagesystems;
 
 import java.io.BufferedReader;
@@ -14,7 +10,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Checks that a capabilities request returns a valid capabilities response.
+ * Checks that a catalog request returns a valid catalog response.
  * @author jbf
  */
 public class CatalogCheck extends Check {
