d031543914b27defad0c56220ae235ac67b5b089
jbf <faden@cottagesystems.com>
2017-06-06 11:20:30 -0500

    trivial fix imports, comments

--- src/java/com/cottagesystems/HapiVerifier.java
+++ src/java/com/cottagesystems/HapiVerifier.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.cottagesystems;
 
-import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.net.MalformedURLException;
 import java.net.URL;
